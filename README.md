3e4702cc7b4394805dfe4ddd84377ea0d042d8d9
Sharun <sharunksplus@gmail.com>
2023-06-04 20:22:33 -0400

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Skeet
 
-[w.i.p]
+Skeet from your command line for [Bluesky](https://bsky.app/) or other supported atproto sites
+
+## Installation
+```
+cargo install skeet
+```
+
+## Environment Setup
+Add the following environment variables:
+```
+SKEET_HOST - optional, defaults to https://bsky.social
+SKEET_USERNAME - your username / handle
+SKEET_PASSWORD - app password. you can create one at https://bsky.app/settings/app-passwords
+```
+
+## Usage
+```
+skeet <your text here>
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use bisky::atproto::ClientBuilder;
+use bisky::bluesky::Bluesky;
+use bisky::lexicon::app::bsky::feed::Post;
+
+#[tokio::main]
+async fn main() {
+    std::panic::set_hook(Box::new(|info| {
+        if let Some(s) = info.payload().downcast_ref::<String>() {
+            println!("{}", s);
+        }
+    }));
+
+    let text = std::env::args().collect::<Vec<String>>()[1..].join(" ");
+
+    let mut client = ClientBuilder::default().build().unwrap();
+
+    client
+        .login(
+            &url::Url::parse(
+                &std::env::var("SKEET_HOST").unwrap_or("https://bsky.social".to_string()),
+            )
+            .unwrap(),
+            &std::env::var("SKEET_USERNAME").expect("Set SKEET_USERNAME as environment variable"),
+            &std::env::var("SKEET_PASSWORD").expect("Set SKEET_PASSWORD as environment variable"),
+        )
+        .await
+        .unwrap();
+
+    let mut bsky = Bluesky::new(client);
+
+    bsky.me()
+        .unwrap()
+        .post(Post {
+            text,
+            created_at: chrono::Utc::now(),
+            rust_type: None,
+            embed: None,
+            reply: None,
+        })
+        .await
+        .unwrap();
 }
